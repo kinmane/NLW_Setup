3e45d399d6fc1a5198bfebb10d1f18fc590f949a
Gabriel Souza <rorato91@gmail.com>
2023-01-19 16:27:24 -0300

    commit
    finalizado configuração inicial server, iniciado web

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
-import Fastify from "fastify"
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'Hello World'
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
 })
 
 app.listen({
   port: 3333,
+}).then(() => {
+  console.log('HTTP Server running')
 })
